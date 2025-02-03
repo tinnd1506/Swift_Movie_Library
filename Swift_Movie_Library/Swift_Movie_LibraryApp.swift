64ad9d53c423ca3490765e7b4d06bf21946bb62b
Alago Victor <123035877+victor-alago@users.noreply.github.com>
2025-02-03 17:11:39 +0100

    Updated the main view

--- Swift_Movie_Library/Swift_Movie_LibraryApp.swift
+++ Swift_Movie_Library/Swift_Movie_LibraryApp.swift
@@ -1,20 +1,14 @@
-//
-//  Swift_Movie_LibraryApp.swift
-//  Swift_Movie_Library
-//
-//  Created by Nguyễn Tín on 03/02/2025.
-//
 
 import SwiftUI
 
 @main
 struct Swift_Movie_LibraryApp: App {
-    let persistenceController = PersistenceController.shared
-
+    @StateObject private var coreDataStore = CoreDataMovieStore()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            MovieListView()  // Update the main view to use coreDataStore
+                .environmentObject(coreDataStore)
         }
     }
 }
